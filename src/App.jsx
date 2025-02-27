ae59b5ea71a30651b206bd95e17e65ad7662786d
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2025-02-27 14:39:59 -0300

    Multiple Courses support

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,59 @@
-import Badge from "react-bootstrap/Badge";
-import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
-import Stack from "react-bootstrap/Stack";
-
-
-const Header = props => (
-    <>
-        <h1 style={{ textAlign: "center" }}>{props.course}</h1>
-    </>
-);
-
-const Part = props => (
-    <ListGroup.Item>
-        {props.part} <Badge bg="secondary">{props.exercises}</Badge>
-    </ListGroup.Item>
-);
-
-const Content = props => (
-    <ListGroup>
-        <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-        <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-        <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </ListGroup>
-);
-
-const Total = props => (
-    <Card>
-        <Card.Body>
-            <Card.Title>
-                <h3>
-                    Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-                </h3>
-            </Card.Title>
-        </Card.Body>
-    </Card>
-);
+import Course from './components/Course'
 
 const App = () => {
-    const course = {
-        name: "Half Stack application development",
-        parts: [
-            {
-                name: "Fundamentals of React",
-                exercises: 10
-            },
-            {
-                name: "Using props to pass data",
-                exercises: 7
-            },
-            {
-                name: "State of a component",
-                exercises: 14
-            }
-        ]
-    };
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1,
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2,
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3,
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4,
+                },
+            ],
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1,
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2,
+                },
+            ],
+        },
+    ]
 
     return (
-        <Stack gap={3} style={{ marginTop: 10 }}>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </Stack>
-    );
-};
+        <>
+            <h1 style={{ textAlign: 'center', marginTop: 30, marginBottom: 30, }}>Web development curriculum</h1>
+            {courses.map((e, i,) => (
+                <Course key={e.name + i} course={e} />
+            ),)}
+        </>
+    )
+}
 
 export default App;
